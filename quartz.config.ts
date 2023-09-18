70b98b93c167fa4cf1a299f4b5934d4b955a98a0
strombraaten <ssb@variant.no>
2023-09-18 22:17:40 +0200

    Quartz sync: Sep 18, 2023, 10:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "www.simenskriver.no",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
