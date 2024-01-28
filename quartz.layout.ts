6f2621d423373b6a115e11f2863884ffe6cb0a22
strombraaten <ssb@variant.no>
2024-01-28 09:06:24 +0100

    Minor changes to the layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,10 +17,10 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.TagList(),
+    // Component.Breadcrumbs(),
     Component.ArticleTitle(),
     // Component.ContentMeta(),
-    Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
